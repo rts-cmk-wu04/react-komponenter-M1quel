a260d5b27d416aed138a996e4ddc0a0fbf952249
Mikkel Frederiksen <5760459@rts-365.dk>
2021-01-11 10:48:32 +0100

    added basic html code

---
+++ src/components/experience/Experience.js
@@ -0,0 +1,25 @@
+
+
+function Experience () {
+    return (
+        <article>
+            <div className="experienceInnerWrapper">
+                <div className="imgWrapper">
+                    <img src="" alt=""/>
+                </div>
+                <section>
+                    <h1>Heading</h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex itaque optio rem sit tempore excepturi!</p>
+                    <div className="imgGallery">
+                        <img src="" alt=""/>
+                        <img src="" alt=""/>
+                        <img src="" alt=""/>
+                    </div>
+                </section>
+            </div>
+        </article>
+    )
+}
+
+
+export default Experience;
