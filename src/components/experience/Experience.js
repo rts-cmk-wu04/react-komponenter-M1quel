37f6e881ad325fc1177e724a85546c94edaea3bd
Mikkel Frederiksen <5760459@rts-365.dk>
2021-01-11 11:44:19 +0100

    added experience card

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,19 +1,34 @@
-
+import React, { useEffect, useState } from "react";
+import "./experience.css"
 
 function Experience () {
+    var [data, setData] = useState({});
+
+	useEffect(function() {
+		fetch("data.json")
+			.then(function(response) {
+				return response.json();
+            })
+            .then(data => {
+                setData(data)
+            })
+	}, []);
+
+    
     return (
-        <article>
+        <article className="experienceWrapper">
             <div className="experienceInnerWrapper">
+                <p className="experienceLeisure">Leisure</p>
                 <div className="imgWrapper">
-                    <img src="" alt=""/>
+                    <img src={ data.experience && data.experience.img } alt=""/>
                 </div>
-                <section>
-                    <h1>Heading</h1>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex itaque optio rem sit tempore excepturi!</p>
+                <section className="experienceContentWrapper">
+                    <h1 className="experienceHeading">{data.experience && data.experience.heading}</h1>
+                    <p className="experiencePara">{ data.experience && data.experience.para }</p>
                     <div className="imgGallery">
-                        <img src="" alt=""/>
-                        <img src="" alt=""/>
-                        <img src="" alt=""/>
+                        <img src={ data.experience && data.experience.imgGallery && data.experience.imgGallery.img1 } alt=""/>
+                        <img src={ data.experience && data.experience.imgGallery && data.experience.imgGallery.img2 } alt=""/>
+                        <img src={ data.experience && data.experience.imgGallery && data.experience.imgGallery.img3 } alt=""/>
                     </div>
                 </section>
             </div>
